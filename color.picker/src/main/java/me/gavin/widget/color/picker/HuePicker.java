ca88120881b554336d5efcd778094c3938ab5e9d
gavin <gavinxxxxxx@gmail.com>
2017-11-27 21:44:36 +0800

    .

--- color.picker/src/main/java/me/gavin/widget/color/picker/HuePicker.java
+++ color.picker/src/main/java/me/gavin/widget/color/picker/HuePicker.java
@@ -2,7 +2,9 @@ package me.gavin.widget.color.picker;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.LinearGradient;
 import android.graphics.Paint;
+import android.graphics.Shader;
 import android.graphics.SweepGradient;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
@@ -25,11 +27,11 @@ public class HuePicker extends View {
             0xFFFF0000,
     };
 
-    private final Paint mHuePaint, mPaint;
+    private final Paint mHuePaint, mSaturationPaint, mLightnessPaint, mAlphaPaint, mPaint;
 
     private float cx, cy, or, ir;
 
-    private final float[] mHSL = {0f, 1f, 0.5f};
+    private final float[] mHSL = {0f, 1f, 0.5f}, mHSLCopyS = new float[3];
 
     private float mRadians = 0f;
 
@@ -46,6 +48,15 @@ public class HuePicker extends View {
         mHuePaint = new Paint(Paint.ANTI_ALIAS_FLAG);
         mHuePaint.setStyle(Paint.Style.STROKE);
 
+        mSaturationPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        mSaturationPaint.setStyle(Paint.Style.STROKE);
+
+        mLightnessPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        mLightnessPaint.setStyle(Paint.Style.STROKE);
+
+        mAlphaPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        mAlphaPaint.setStyle(Paint.Style.STROKE);
+
         mPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
         mPaint.setColor(ColorUtils.HSLToColor(mHSL));
     }
@@ -58,6 +69,35 @@ public class HuePicker extends View {
         ir = or - 24f;
         mHuePaint.setStrokeWidth(or - ir);
         mHuePaint.setShader(new SweepGradient(cx, cy, COLORS, null));
+
+        System.arraycopy(mHSL, 0, mHSLCopyS, 0, 3);
+        mHSLCopyS[1] = 0f;
+        int startColor = ColorUtils.HSLToColor(mHSLCopyS);
+        mHSLCopyS[1] = .5f;
+        int cColor = ColorUtils.HSLToColor(mHSLCopyS);
+        mHSLCopyS[1] = 1f;
+        int endColor = ColorUtils.HSLToColor(mHSLCopyS);
+        mSaturationPaint.setStrokeWidth(24);
+        mSaturationPaint.setShader(new LinearGradient(38, 0, w - 38, 0,
+                new int[]{startColor, cColor, endColor}, null, Shader.TileMode.REPEAT));
+
+        System.arraycopy(mHSL, 0, mHSLCopyS, 0, 3);
+        mHSLCopyS[2] = 0f;
+        startColor = ColorUtils.HSLToColor(mHSLCopyS);
+        mHSLCopyS[2] = .5f;
+        cColor = ColorUtils.HSLToColor(mHSLCopyS);
+        mHSLCopyS[2] = 1f;
+        endColor = ColorUtils.HSLToColor(mHSLCopyS);
+        mLightnessPaint.setStrokeWidth(24);
+        mLightnessPaint.setShader(new LinearGradient(38, 0, w - 38, 0,
+                new int[]{startColor, cColor, endColor}, null, Shader.TileMode.REPEAT));
+
+        System.arraycopy(mHSL, 0, mHSLCopyS, 0, 3);
+        int color = ColorUtils.HSLToColor(mHSLCopyS);
+        mAlphaPaint.setStrokeWidth(24);
+        mAlphaPaint.setShader(new LinearGradient(38, 0, w - 38, 0,
+                color & 0x00FFFFFF, color | 0xFF000000, Shader.TileMode.REPEAT));
+
     }
 
     @Override
@@ -71,6 +111,10 @@ public class HuePicker extends View {
         float x = (float) (cx + (or + ir) / 2 * Math.cos(radian));
         float y = (float) (cy - (or + ir) / 2 * Math.sin(radian));
         canvas.drawCircle(x, y, 48f, mPaint);
+
+        canvas.drawLine(32, 1800, getWidth() - 32, 1800, mSaturationPaint);
+        canvas.drawLine(32, 1900, getWidth() - 32, 1900, mLightnessPaint);
+        canvas.drawLine(32, 2000, getWidth() - 32, 2000, mAlphaPaint);
     }
 
     @Override
@@ -78,7 +122,7 @@ public class HuePicker extends View {
         switch (event.getAction()) {
             case MotionEvent.ACTION_DOWN:
                 double distance = Math.sqrt(Math.pow(cx - event.getX(), 2) + Math.pow(cy - event.getY(), 2));
-                if (ir - 20 < distance && distance < or + 20) {
+                if (ir - 32 < distance && distance < or + 32) {
                     mHSL[0] = getAngle(event.getX() - cx, event.getY() - cy);
                     invalidate();
                     return true;
